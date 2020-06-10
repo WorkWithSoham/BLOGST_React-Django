7e358ced5a9e9acb8b2c1ba9ac2185bf3940b620
WorkWithSoham <64754133+WorkWithSoham@users.noreply.github.com>
2020-06-10 22:10:35 +0530

    Add files via upload

---
+++ BLOGST/rest_api/dev.py
@@ -0,0 +1,11 @@
+from rest_framework import authentication
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class DevAuthentication(authentication.BasicAuthentication):
+    def authenticate(self, request):
+        qs = User.objects.filter(id=1)
+        user = qs.order_by("?").first()
+        return (user, None)
